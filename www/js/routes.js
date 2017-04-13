836e3e3f53ef3a4aec771e4d7e1ea9fb3491bf29
Fábio Amarante <fabioamarante@hotmail.com>
2017-04-13 15:25:06 +0100

    1st commit

---
+++ www/js/routes.js
@@ -0,0 +1,89 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+  
+
+      .state('menu.home', {
+    url: '/home-page',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/home.html',
+        controller: 'homeCtrl'
+      }
+    }
+  })
+
+  .state('cloud', {
+    url: '/page3',
+    templateUrl: 'templates/cloud.html',
+    controller: 'cloudCtrl'
+  })
+
+  .state('menu', {
+    url: '/side-menu21',
+    templateUrl: 'templates/menu.html',
+    controller: 'menuCtrl'
+  })
+
+  .state('menu.signup', {
+    url: '/signup-page',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/signup.html',
+        controller: 'signupCtrl'
+      }
+    }
+  })
+
+  .state('menu.login', {
+    url: '/login-page',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/login.html',
+        controller: 'loginCtrl'
+      }
+    }
+  })
+
+  .state('menu.myProfile', {
+    url: '/myprofile-page',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/myProfile.html',
+        controller: 'myProfileCtrl'
+      }
+    }
+  })
+
+  .state('menu.editProfile', {
+    url: '/editprofile-page',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/editProfile.html',
+        controller: 'editProfileCtrl'
+      }
+    }
+  })
+
+  .state('menu.preferences', {
+    url: '/preferences',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/preferences.html',
+        controller: 'preferencesCtrl'
+      }
+    }
+  })
+
+$urlRouterProvider.otherwise('/side-menu21/home-page')
+
+  
+
+});
