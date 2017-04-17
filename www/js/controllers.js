81d5e6594c313ea355037f62d18b9b7355e28906
Fábio Amarante <fabioamarante@hotmail.com>
2017-04-17 16:12:18 +0100

    started work on home page filters

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,17 +1,111 @@
 angular.module('app.controllers', [])
 
-.controller('homeCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('homeCtrl', ['$scope', '$stateParams', '$cordovaGeolocation', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
+function ($scope, $stateParams, $cordovaGeolocation) {
+  $scope.selectedCategories = [];
+  $scope.distanceValue = 5;
+  $scope.categories = [{id: 1, value: "Igrejas"}, {id: 2, value: "Museus"}, {id: 3, value: "Pontes"}];
 
-  $scope.data = [{id: 1, value: "Igrejas"}, {id: 2, value: "Museus"}, {id: 3, value: "Pontes"}];
-  //$scope.default=["Item 2"];
   $scope.onValueChanged = function(value){
-    console.log(value);
+    $scope.selectedCategories = value;
   }
 
-  $scope.distanceValue = 5;
+  $scope.onDistanceChanged = function(value){
+    $scope.distanceValue = value;
+  }
+
+  $scope.searchClick = function () {
+    console.log($scope.distanceValue);
+    console.log($scope.selectedCategories);
+    $scope.items = [];
+    initMap($scope.selectedCategories);
+  }
+
+  var map;
+  var infowindow;
+
+  var options = {timeout: 10000, enableHighAccuracy: true};
+
+  $cordovaGeolocation.getCurrentPosition(options).then(function(position){
+
+    $scope.position = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
+  }, function(error){
+    console.log("Could not get location");
+  });
+
+  function initMap(categories) {
+    var church = false;
+    var museum = false;
+    var other = false;
+
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: $scope.position,
+      zoom: 15
+    });
+
+    for(var i = 0; i < categories.length; i++){
+      if(categories[i].id == 1){
+        church = true;
+      }else if(categories[i].id == 2){
+        museum = true;
+      }else{
+
+      }
+    }
+
+    var request_churches = {
+      location: $scope.position,
+      radius: $scope.distanceValue * 1000,
+      query: 'igreja',
+      type: 'church'
+    };
+
+    var request_museums = {
+      location: $scope.position,
+      radius: $scope.distanceValue * 1000,
+      query: 'museu',
+      type: 'museum'
+    };
+
+    /*var request_bridges = {
+      location: porto,
+      radius: 5000,
+      query: 'ponte'
+    };
+    */
+
+    infowindow = new google.maps.InfoWindow();
+    var service = new google.maps.places.PlacesService(map);
+
+    //Call text search for churches
+    if(church)
+      service.textSearch(request_churches, callback);
+
+    //Call text search for museums
+    if(museum)
+      service.textSearch(request_museums, callback);
+
+  }
+
+  function callback(results, status) {
+    if (status === google.maps.places.PlacesServiceStatus.OK) {
+      console.log(results);
+      for (var i = 0; i < results.length; i++) {
+        if(results[i].photos != null){
+          var item = {name: results[i].name, rating: results[i].rating, photo: results[i].photos[0].getUrl({'maxWidth': 300, 'maxHeight': 300})};
+        }
+        else{
+          var item = {name: results[i].name, rating: results[i].rating, photo: null};
+        }
+        $scope.items.push(item);
+      }
+      $scope.$apply();
+      console.log($scope.items);
+    }
+  }
 }])
 
 .controller('cloudCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
